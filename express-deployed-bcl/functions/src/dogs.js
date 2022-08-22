cacf87a1f6effeaf8598c7fd80abe5234f475ce7
Bridgette Lemus <bridgette.lemus.se@gmail.com>
2022-08-22 15:35:04 -0400

    added another dog

--- express-deployed-bcl/functions/src/dogs.js
+++ express-deployed-bcl/functions/src/dogs.js
@@ -2,6 +2,7 @@ export function getDogs(req, res) {
   res.send([
     { name: "Ryder", age: 4, breed: "Dumbass", gender: "MN" },
     { name: "Duke", age: 2, breed: "Rottwiller", gender: "M" },
+    { name: "Indigo", age: 10, breed: "Great Dane/ Lab Mix", gender: "M"},
   ]);
 }
 
